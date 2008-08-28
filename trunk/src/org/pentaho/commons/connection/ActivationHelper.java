a4b604f6f8af8dca9f6b9dfd0e8a25de75d78da7
Davide Kincade <dkincade@pentaho.com>
2008-08-28 00:41:57 +0000

    Moving the existing trunk/pentaho-connections project to pentaho-connections/trunk
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-connections@31436 5d404267-db13-0410-8476-b1f6b398e63a

---
+++ trunk/src/org/pentaho/commons/connection/ActivationHelper.java
@@ -0,0 +1,61 @@
+package org.pentaho.commons.connection;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.activation.DataSource;
+
+public class ActivationHelper {
+  
+  public static final class DatasourceWrapper implements IPentahoStreamSource {
+    private DataSource dataSource;
+    public DatasourceWrapper(DataSource ds) {
+      super();
+      assert (ds != null);
+      this.dataSource = ds;
+    }
+    public String getContentType() {
+      return dataSource.getContentType();
+    }
+
+    public InputStream getInputStream() throws IOException {
+      return dataSource.getInputStream();
+    }
+
+    public String getName() {
+      return dataSource.getName();
+    }
+
+    public OutputStream getOutputStream() throws IOException {
+      return dataSource.getOutputStream();
+    }
+  }
+
+  public static final class PentahoStreamSourceWrapper implements DataSource {
+    private IPentahoStreamSource streamSource;
+
+    public PentahoStreamSourceWrapper(IPentahoStreamSource ss) {
+      super();
+      assert ss != null;
+      this.streamSource = ss;
+    }
+    public String getContentType() {
+      return streamSource.getContentType();
+    }
+
+    public InputStream getInputStream() throws IOException {
+      return streamSource.getInputStream();
+    }
+
+    public String getName() {
+      return streamSource.getName();
+    }
+
+    public OutputStream getOutputStream() throws IOException {
+      return streamSource.getOutputStream();
+    }
+  }
+  
+
+}
---
+++ trunk/src/org/pentaho/commons/connection/IPentahoMetaData.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2006 Pentaho Corporation.  All rights reserved. 
+ * This software was developed by Pentaho Corporation and is provided under the terms 
+ * of the Mozilla Public License, Version 1.1, or any later version. You may not use 
+ * this file except in compliance with the license. If you need a copy of the license, 
+ * please go to http://www.mozilla.org/MPL/MPL-1.1.txt. The Original Code is the Pentaho 
+ * BI Platform.  The Initial Developer is Pentaho Corporation.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS" 
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to 
+ * the license for the specific language governing your rights and limitations.
+ *
+ * Created Sep 7, 2005 
+ * @author wseyler
+ */
+
+package org.pentaho.commons.connection;
+
+/**
+ * Defines the methods that must be supported for obtaining metadata about
+ * a source of data. 
+ * 
+ * @author wseyler
+ */
+public interface IPentahoMetaData {
+
+  /**
+   * @return a 2D grid that represents column headers.
+   * 
+   * NOTE: 2D data will contain a column header that is 1 x N where N is the
+   * number of columns in the data. Multidimensional data will return N x M
+   * where N is the number of dimensions and M is the number of columns. With
+   * the 0 index for N representing the innermost dimension.
+   */
+  public Object[][] getColumnHeaders();
+
+  /**
+   * @return a 2D grid that represents row headers.
+   * 
+   * NOTE: 2D data will return null for the row header. Multidimensional data
+   * will return N x M where M is the number of dimensions and N is the number
+   * of rows. With the 0 index for M representing the innermost dimension.
+   */
+  public Object[][] getRowHeaders();
+
+  /**
+   * Gets the 0-based column number for a specified column header value. If
+   * the column header cannot be found -1 is returned. If there is more than 1
+   * column header dimentsions -1 is returned.
+   * 
+   * @param value
+   *            The column header value to search for.
+   * @return The 0-based index of the column
+   * @throws Exception
+   */
+  public int getColumnIndex(String value);
+
+  /**
+   * Gets the 0-based column number for a set of column header values. If the
+   * column header cannot be found -1 is returned. If the number of column
+   * header dimensions does not match the number of values provided -1 is
+   * returned.
+   * 
+   * @param value
+   *            The column header value to search for.
+   * @return The 0-based index of the column
+   */
+  public int getColumnIndex(String[] values);
+
+  /**
+   * Gets the 0-based row number for a specified row header value. If the row
+   * header cannot be found -1 is returned. If there is more than 1 row header
+   * dimentsions -1 is returned.
+   * 
+   * @param value
+   *            The row header value to search for.
+   * @return The 0-based index of the row
+   * @throws Exception
+   */
+  public int getRowIndex(String value);
+
+  /**
+   * Gets the 0-based row number for a set of row header values. If the row
+   * header cannot be found -1 is returned. If the number of row header
+   * dimensions does not match the number of values provided -1 is returned.
+   * 
+   * @param value
+   *            The row header value to search for.
+   * @return The 0-based index of the row
+   */
+  public int getRowIndex(String[] values);
+
+  /**
+   * @return Count of columns returned.
+   */
+  public int getColumnCount();
+  
+  /**
+   * get an attribute from the metadata
+   * 
+   * @param rowNo the row number
+   * @param columnNo the column number
+   * @param attributeName the attribute name
+   * @return the attribute
+   */
+  public Object getAttribute(int rowNo, int columnNo, String attributeName);
+}
