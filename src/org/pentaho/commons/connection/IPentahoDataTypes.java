b45bfff36387807a8d1edcd42febbda589be2416
James Dixon <jdixon@pentaho.com>
2009-02-06 21:24:45 +0000

    BISERVER-2741 Need new web services infrastrcture to support current projects
    Moved serializable result set classes to commons connection from the webservice core project.
    
    git-svn-id: svn://source.pentaho.org/svnroot/pentaho-commons/pentaho-connections/trunk@36123 5d404267-db13-0410-8476-b1f6b398e63a

--- src/org/pentaho/commons/connection/IPentahoDataTypes.java
+++ src/org/pentaho/commons/connection/IPentahoDataTypes.java
@@ -30,4 +30,25 @@ public interface IPentahoDataTypes {
   public static final int AXIS_COLUMN = 0;
 
   public static final int AXIS_ROW = 1;
+  
+  // these type names are taken from w3c XML Schema data types
+  
+  public static final String TYPE_STRING = "string"; //$NON-NLS-1$
+  
+  public static final String TYPE_DOUBLE = "double"; //$NON-NLS-1$
+  
+  public static final String TYPE_FLOAT = "float"; //$NON-NLS-1$
+  
+  public static final String TYPE_INT = "integer"; //$NON-NLS-1$
+  
+  public static final String TYPE_DECIMAL = "decimal"; //$NON-NLS-1$
+  
+  public static final String TYPE_DATE = "dateTime"; //$NON-NLS-1$
+  
+  public static final String TYPE_LONG = "long"; //$NON-NLS-1$
+  
+  public static final String TYPE_BOOLEAN = "boolean"; //$NON-NLS-1$
+  
+  public static final String DATE_FORMAT = "yyyy-MM-dd.HH:mm:ss"; //$NON-NLS-1$
+
 }
