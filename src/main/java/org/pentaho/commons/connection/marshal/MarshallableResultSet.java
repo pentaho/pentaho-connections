03d3aa6dce52930fc0026b5b8696bd30f93b41a7
SahithiKommagani6 <sahithi.kommagani@hitachivantara.com>
2024-10-21 16:40:11 +0530

    [BACKLOG-41287]-Upgrading pentaho-connections from Java EE to Jakarta EE to support with Tomcat-10.X

--- src/main/java/org/pentaho/commons/connection/marshal/MarshallableResultSet.java
+++ src/main/java/org/pentaho/commons/connection/marshal/MarshallableResultSet.java
@@ -16,7 +16,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import org.pentaho.commons.connection.IPentahoDataTypes;
 import org.pentaho.commons.connection.IPentahoMetaData;
